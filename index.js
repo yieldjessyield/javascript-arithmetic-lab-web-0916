05b15c99a47b269a0205ebab6e9b2cbb1235d8d0
Jess <jess.harrelson0707@gmail.com>
2016-11-02 19:22:06 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a, b){
+  var result = a + b
+  return result
+}
+
+function subtract(a, b){
+  var result = a - b
+  return result
+}
+
+function multiply(a, b){
+  var result = a * b
+  return result
+}
+
+function divide(a, b){
+  var result = a / b
+  return result
+}
+
+function inc(n){
+  n++
+  var result = n
+  return result
+}
+
+function dec(n){
+  n--
+  var result = n
+  return result
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+
+
+}
+
+function preserveDecimal(n){
+  return parseFloat(n, 10)
+
+}
